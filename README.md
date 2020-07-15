96d7abb0025bfd26f21c31aeed753d930cfe9724
Gunnar Lieb <gunnar.lieb@akitogo.com>
2020-07-15 11:53:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Akitogo Melissa Address Validator
+
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Akitogo_MelissaAddressValidator',
+    __DIR__
+);
